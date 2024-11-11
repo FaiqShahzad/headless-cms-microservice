ed8135f856e39c07166b8635519fae3adaf51925
Faiq Shahzad <faiqshahzad.dev@boolmind.com>
2024-11-11 19:33:33 +0500

    Separated grpc service and api-gateway into different projects

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { HeroModule } from './api-gateway/hero/hero.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HeroesServiceModule } from './hero/heroesService.module';
 
 @Module({
-  imports: [HeroModule, HeroesServiceModule],
+  imports: [HeroesServiceModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,16 @@ import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.GRPC,
-    options: {
-      package: 'hero',
-      protoPath: join(__dirname, 'hero/hero.proto'),
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'hero',
+        protoPath: join(__dirname, 'proto/hero.proto'),
+      },
     },
-  });
-  await app.startAllMicroservices();
-  await app.listen(process.env.PORT ?? 3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  );
+  await app.listen();
 }
 bootstrap();
