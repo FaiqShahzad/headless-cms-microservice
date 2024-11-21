da549119e0c72d5e42c9caa2f14c1f1ce1a02dc2
Faiq Shahzad <faiqshahzad.dev@gmail.com>
2024-11-21 18:13:08 +0500

    Update dockerignore and set db password

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,7 @@ import { UserModule } from './user/user.module';
         host: configService.get('POSTGRES_HOST'),
         port: configService.get<number>('POSTGRES_PORT'),
         username: configService.get('POSTGRES_USER'),
-        // password: configService.get('POSTGRES_PASSWORD'),
+        password: configService.get('POSTGRES_PASSWORD'),
         database: configService.get('POSTGRES_DB'),
         entities: [Users],
         synchronize: true,
